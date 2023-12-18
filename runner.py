c297c2dde0c58e7152cbcb945c46970552a2f406
gluck <glucksistemi@gmail.com>
2023-12-18 13:45:18 +0300

    added CMD preparation

--- runner.py
+++ runner.py
@@ -4,7 +4,7 @@ import os.path
 from worker_framework import Runner, TaskTransfer
 from worker_framework.tools.workdir import CreateDirMode
 from worker_framework.message_managers import HttpPollMsgManager
-
+from pymol import cmd
 from packer.mutation import MutationPacker
 import requests
 
@@ -22,9 +22,12 @@ class ModificationRunner(Runner):
         if task.model_id > 0:
             task.model_id -= 1
         in_path = os.path.join(task.workdir, 'reference.pdb')
-
         with open(in_path, 'w') as out_file:
             out_file.write(requests.get(task.url).text)
+        cmd.reinitialize()
+        cmd.load(in_path)
+        cmd.remove('not polymer')
+        cmd.save(in_path)
 
         packer = MutationPacker()
         packer.load_from_pdb(in_path)
