334cb7e7dbd0cc90836dd0a5a2bf2f5d350a8c09
gluck <glucksistemi@gmail.com>
2024-05-18 07:33:39 +0300

    WT 0.2.3

--- run_params.sh
+++ run_params.sh
@@ -6,11 +6,13 @@ IMAGE_BASE_NAME="scpacker_modification"
 declare -A DOCKER_PARAMS
 # launch parameters for every environment
 DOCKER_PARAMS["localhost"]="--shm-size=4G"
+DOCKER_PARAMS["standalone"]="--shm-size=4G"
 DOCKER_PARAMS["test"]="--shm-size=4G"
 
 declare -A VOLUMES
 # volume setup for every environment
 VOLUMES["localhost"]="-v /home/gluck/modif_data:/data"
+VOLUMES["standalone"]="-v /home/gluck/ptm_test_1:/data"
 VOLUMES["test"]="-v /root/workers/test_data:/data"
 
 declare -A COMMANDS
--- runner.py
+++ runner.py
@@ -4,7 +4,6 @@ import os.path
 from worker_framework import Runner, TaskTransfer
 from worker_framework.tools.workdir import CreateDirMode
 from worker_framework.message_managers import HttpPollMsgManager
-from worker_framework.tools.recorder import Recorder
 from pymol import cmd
 from packer.mutation import MutationPacker
 import requests
@@ -19,36 +18,56 @@ class ModificationRunner(Runner):
     msg_manager_class = HttpPollMsgManager
     worker_name = 'scpacker'
 
-    def handler(self, task: TaskTransfer) -> TaskTransfer:
-        logging.debug(task.json())
-        self.stage_writer.write_stage('download', 'start')
-        if task.model_id > 0:
-            task.model_id -= 1
-        in_path = os.path.join(task.workdir, 'reference.pdb')
-        with open(in_path, 'w') as out_file:
-            out_file.write(requests.get(task.url).text)
-        cmd.reinitialize()
-        cmd.load(in_path)
-        cmd.remove('not polymer')
-        cmd.save(in_path)
-        self.stage_writer.write_stage('download', 'finish', 'OK')
-        logging.debug('Initiating packer')
-        self.stage_writer.write_stage('mutation', 'start')
-        rec = Recorder()
-        rec.start()
+    @staticmethod
+    def run_mutation(in_path, chain, code, no, out_path):
         packer = MutationPacker()
         packer.load_from_pdb(in_path)
         packer.mutate(
-            aa_code=task.amino_replacements[0].replace,
-            chain=task.apfid[5],
-            site=task.amino_replacements[0].no,
-            n_samples = 200,
-            with_library = True,
-            repack_mode = 'MCMC'  # MCMC, GA
+            aa_code=code,
+            chain=chain,
+            site=no,
+            n_samples=200,
+            with_library=True,
+            repack_mode='MCMC'  # MCMC, GA
         )
-        packer.save(os.path.join(task.workdir, 'modified.pdb'))
-        rec.stop()
-        self.stage_writer.write_stage('mutation', 'finish', 'OK', rec.get_result())
+        packer.save(out_path)
+
+    def handler(self, task: TaskTransfer) -> TaskTransfer:
+        logging.debug(task.json())
+        with self.stage_writer('download'):
+            if task.model_id > 0:
+                task.model_id -= 1
+            raw_path = os.path.join(task.workdir, 'raw.pdb')
+            in_path = os.path.join(task.workdir, 'backbone.pdb')
+            with open(raw_path, 'w') as out_file:
+                out_file.write(requests.get(task.url).text)
+            cmd.reinitialize()
+            cmd.load(raw_path)
+            cmd.remove('not polymer')
+            cmd.remove('elem H')
+            cmd.remove('not backbone and resn HIS')
+            resns = []
+            cmd.iterate(f'resi {task.amino_replacements[0].no} and n. CA', lambda atom: resns.append(atom.resn))
+            source_residue = resns[0]
+            cmd.save(in_path)
+
+        logging.debug('Initiating packer')
+        with self.stage_writer('refine_intact', True):
+            self.run_mutation(
+                in_path,
+                task.apfid[5],
+                source_residue,
+                task.amino_replacements[0].no,
+                os.path.join(task.workdir, 'reference.pdb')
+            )
+        with self.stage_writer('apply_modification', True):
+            self.run_mutation(
+                in_path,
+                task.apfid[5],
+                task.amino_replacements[0].replace,
+                task.amino_replacements[0].no,
+                os.path.join(task.workdir, 'modified.pdb')
+            )
         return task
 
 
